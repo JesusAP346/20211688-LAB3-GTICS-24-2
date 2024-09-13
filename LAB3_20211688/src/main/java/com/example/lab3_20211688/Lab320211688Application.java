aa8a2dc54259c34e471ee1d1f94831b8c3d8eba1
Jesus Alvarado <a2021188@pucp.edu.pe>
2024-09-12 19:19:27 -0500

    primer commit

---
+++ LAB3_20211688/src/main/java/com/example/lab3_20211688/Lab320211688Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20211688;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320211688Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320211688Application.class, args);
+    }
+
+}
---
+++ LAB3_20211688/src/test/java/com/example/lab3_20211688/Lab320211688ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20211688;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320211688ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
